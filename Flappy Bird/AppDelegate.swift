d34366e43aa89f0401086e8cde4428fa0ae42486
Brandon Plank <52553007+brandonplank@users.noreply.github.com>
2020-05-04 23:52:15 -0500

    killswitch

--- Flappy Bird/AppDelegate.swift
+++ Flappy Bird/AppDelegate.swift
@@ -13,6 +13,23 @@ import AVFoundation
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        //kill switch just in case of a copyright notice
+        if let url = URL(string: "https://flappyapp.org/hdsgaukfgjhdsghugujyadsgluyfgljasglfjsdgjfdgdsghgudsaiguyfguifegiutfgaitdgfyiauifudsyguasygbfyasguykdfaegbwkjrfbkjagbfutcwegautrfuwtbfuwtbeutirfiutawtgbuifyhusirefbguiygeryfysgfyusgeoyiifegyryiegufygruifysigeyigfes/killswitch.txt") {
+            do {
+                let contents = try String(contentsOf: url)
+                print(contents)
+                if contents == "yes\n"{
+                    print("The killswitch is active")
+                    exit(0)
+                } else {
+                    print("The killswitch is not active")
+                }
+            } catch {
+                print("Failed to load the killswitch address. Not doing anything.")
+            }
+        } else {
+           print("Failed to load the killswitch address. Not doing anything.")
+        }
         try? AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.ambient)
         return true
     }
